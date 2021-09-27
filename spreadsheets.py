855671db0581a1b7f97d20adb2bdee6a9e40a232
Aaron Watt <aaron@acwatt.net>
2021-09-27 08:55:12 -0700

    Add script for handling spreadsheets

---
+++ spreadsheets.py
@@ -0,0 +1,34 @@
+# Python 3.7
+# File name: 
+# Authors: Aaron Watt
+# Date: 2021-07-28
+""""""
+# Standard library imports
+from pathlib import Path
+
+# Third-party imports
+
+# Local application imports
+
+
+# FUNCTIONS --------------------------
+utitlities_dir = Path(r"D:\Documents\kcgcGoogleDrive\College_future\Housing\The Jungle on Ellsworth\Utilities")
+embud_dir = utitlities_dir / 'ebmud'
+PGE_dir = utitlities_dir / 'PGE'
+sonic_dir = utitlities_dir / 'sonic'
+
+ethernet_calc_path = r"...\The Jungle on Ellsworth\internet\interest on internet expenses.xlsx"
+bills_csv = utitlities_dir / 'bills.csv'
+charges_csv = utitlities_dir / 'charges.csv'
+usernames_csv = utitlities_dir / 'usernames.csv'
+
+
+
+# MAIN -------------------------------
+if __name__ == "__main__":
+    pass
+
+# REFERENCES -------------------------
+"""
+
+"""
